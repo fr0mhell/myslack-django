f0f524e719e24a694512aa0c96d0df81d395b81e
fr0mhell <t1m0feev.grigorij@gmail.com>
2022-12-10 17:36:01 +0300

    Add Swagger generation

--- myslack/api/urls.py
+++ myslack/api/urls.py
@@ -9,44 +9,44 @@ from .views.workspaces import ProfileViewSet, WorkspaceViewSet
 # Create a router and register our viewsets with it.
 v1_router = DefaultRouter()
 # Top-level API
-v1_router.register('workspaces/', WorkspaceViewSet, basename='workspaces')
-v1_router.register('reactions/', ReactionViewSet, basename='reactions')
+v1_router.register('workspaces', WorkspaceViewSet, basename='workspaces')
+v1_router.register('reactions', ReactionViewSet, basename='reactions')
 # Workspace-related API
 v1_router.register(
-    r'workspaces/(?P<workspace_id>\d+)/profiles/',
+    r'workspaces/(?P<workspace_id>\d+)/profiles',
     ProfileViewSet,
     basename='profiles',
 )
 v1_router.register(
-    r'workspaces/(?P<workspace_id>\d+)/channels/',
+    r'workspaces/(?P<workspace_id>\d+)/channels',
     ChannelViewSet,
     basename='channels',
 )
 # Channel-related API
 v1_router.register(
-    r'workspaces/(?P<workspace_id>\d+)/channels/(?P<channel_id>\d+)/threads/',
+    r'workspaces/(?P<workspace_id>\d+)/channels/(?P<channel_id>\d+)/threads',
     ThreadViewSet,
     basename='threads',
 )
 # Thread-related API
 v1_router.register(
-    r'workspaces/(?P<workspace_id>\d+)/channels/(?P<channel_id>\d+)/threads/(?P<thread_id>\d+)/comments/',
+    r'workspaces/(?P<workspace_id>\d+)/channels/(?P<channel_id>\d+)/threads/(?P<thread_id>\d+)/comments',
     CommentViewSet,
     basename='comments',
 )
 # TODO: probably don't need as separate viewsets
 v1_router.register(
-    'thread-reactions/',
+    'thread-reactions',
     ThreadReactionViewSet,
     basename='thread-reactions',
 )
 v1_router.register(
-    'comment-reactions/',
+    'comment-reactions',
     CommentReactionViewSet,
     basename='comment-reactions',
 )
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('v1/', include(v1_router.urls)),
+    path('v1', include(v1_router.urls)),
 ]
